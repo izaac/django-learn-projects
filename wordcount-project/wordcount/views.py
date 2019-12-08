5dc0f551ee7beffcb123c11d20a82fe214156fee
Izaac Zavaleta <izaac@rancher.com>
2019-12-08 11:29:56 -0700

    adding html templates and forms lesson

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, 'home.html', {'hithere': 'this is our home.'})
+    return render(request, 'home.html')
 
 
-def eggs(request):
-    return HttpResponse('<h1> Eggs are great </h1>')
+def count(request):
+    return render(request, 'count.html')
