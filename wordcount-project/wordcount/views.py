10a109599df7d6633d2c6396dc7ec91887ed79fe
Izaac Zavaleta <izaac@rancher.com>
2019-12-09 17:35:28 -0700

    improving word count

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,3 +1,5 @@
+import operator
+
 from django.shortcuts import render
 
 
@@ -7,4 +9,14 @@ def homepage(request):
 
 def count(request):
     fulltext = request.GET['fulltext']
-    return render(request, 'count.html', {'fulltext': fulltext})
+
+    wordlist = fulltext.split()
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            worddictionary[word] = worddictionary[word] + 1
+        else:
+            worddictionary[word] = 1
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'sortedwords': sortedwords, 'fulltext': fulltext, 'count': len(wordlist)})
