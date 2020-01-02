634e9e617cff4682db08aec4bb76de4528c2f5f1
Izaac Zavaleta <izaac@rancher.com>
2020-01-01 21:55:05 -0700

    adding initial processing of blog post details

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -6,3 +6,8 @@ from .models import Blog
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    detail_post = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'post': detail_post})
