c2edaefa08790fcbaf73c440843a4c118a946681
Izaac Zavaleta <izaac@rancher.com>
2020-01-03 16:28:24 -0700

    doing some polish

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allblogs, name='home'),
+    path('', views.allblogs, name='allblogs'),
     path('<int:blog_id>/', views.detail, name='detail')
 ]
